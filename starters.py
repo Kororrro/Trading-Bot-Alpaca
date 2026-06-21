6ae6d85f2d7c1f2b0a3b00e18809e7f0cd4eb755
Kororrro <korofm40@gmail.com>
2026-06-21 20:38:37 +0200

    changes

---
+++ starters.py
@@ -0,0 +1,12 @@
+import starters
+
+"""
+Market: ETH/USD
+Entry: if price is less than yesterday
+Amount: const 3 qty
+Risk: 1.5%
+Exit: stop-loss 2% less than price i bought 
+Filters: 
+Execution: execution
+Validation: none
+"""
